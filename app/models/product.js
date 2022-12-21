aed8c607e88fca3de8b42b11accb192a68923851
afsarahmad786 <mohammadafsar415@gmail.com>
2022-12-21 19:56:32 +0530

    relations

--- app/models/product.js
+++ app/models/product.js
@@ -1,37 +1,27 @@
-const Sequelize = require("sequelize");
-// import { DataTypes } from "sequelize";
-const sequelize = require("../util/database");
+const Sequelize = require('sequelize');
 
-const Product = sequelize.define("product", {
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product', {
   id: {
-    // type: Sequelize.DataTypes.INTEGER,
-
-    // type: Sequelize.INTEGER,
-    type: Sequelize.DataTypes.INTEGER,
-    primaryKey: true,
+    type: Sequelize.INTEGER,
     autoIncrement: true,
-    // allowNull: false,
-
-    // primaryKey: true,
-    // autoIncrement: true,
-    // type: Sequelize.INTEGER,
-    // autoIncrement: true,
-    // allowNull: false,
-    // primaryKey: true
+    allowNull: false,
+    primaryKey: true
   },
   title: Sequelize.STRING,
   price: {
     type: Sequelize.DOUBLE,
-    allowNull: false,
+    allowNull: false
   },
   imageUrl: {
     type: Sequelize.STRING,
-    allowNull: false,
+    allowNull: false
   },
   description: {
     type: Sequelize.STRING,
-    allowNull: false,
-  },
+    allowNull: false
+  }
 });
 
 module.exports = Product;
